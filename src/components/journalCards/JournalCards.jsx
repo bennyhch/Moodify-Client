5bfa24ab861661685339054e708538b1112ec940
Ben Ho <bennyhch@gmail.com>
2022-08-01 17:36:31 +0100

    accordion rendered successfully

---
+++ src/components/journalCards/JournalCards.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import moment from "moment";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useSelector } from "react-redux";
+
+const JournalCards = ({ emotion, imageSrc }) => {
+  const { allEvent } = useSelector((store) => store.event);
+  console.log("allevent", allEvent);
+  const eventsByEmo = allEvent.filter((event) => event.emotions === emotion);
+  // console.log("eventsbyemo", eventsByEmo);
+
+  return (
+    <div>
+      <p>{emotion}</p>
+      <img src={imageSrc} alt={`${emotion}-emoji`} />
+
+      {/* incident */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Incident</Typography>
+        </AccordionSummary>
+        {eventsByEmo.map((el) => {
+          const { incident, timeOfEvent } = el;
+          return (
+            <AccordionDetails>
+              <Typography>
+                <div>{incident} </div>
+                <div>
+                  {moment(timeOfEvent).format("MMMM Do YYYY, h:mm:ss a")}
+                </div>
+              </Typography>
+            </AccordionDetails>
+          );
+        })}
+      </Accordion>
+      {/* thought */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Thought</Typography>
+        </AccordionSummary>
+        {eventsByEmo.map((el) => {
+          const { thought, timeOfEvent } = el;
+          return (
+            <AccordionDetails>
+              <Typography>
+                <div>{thought} </div>
+                <div>
+                  {moment(timeOfEvent).format("MMMM Do YYYY, h:mm:ss a")}
+                </div>
+              </Typography>
+            </AccordionDetails>
+          );
+        })}
+      </Accordion>
+      {/* behavior */}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Behavior</Typography>
+        </AccordionSummary>
+        {eventsByEmo.map((el) => {
+          const { behavior, timeOfEvent } = el;
+          return (
+            <AccordionDetails>
+              <Typography>
+                <div>{behavior} </div>
+                <div>
+                  {moment(timeOfEvent).format("MMMM Do YYYY, h:mm:ss a")}
+                </div>
+              </Typography>
+            </AccordionDetails>
+          );
+        })}
+      </Accordion>
+    </div>
+  );
+};
+
+export default JournalCards;
--- src/pages/journal/Journal.jsx
+++ src/pages/journal/Journal.jsx
@@ -1,62 +1,25 @@
-import React, { useEffect } from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useDispatch } from "react-redux";
+import JournalCards from "../../components/journalCards/JournalCards";
 import { getAllEvents } from "../../features/eventSlice";
+import emojiItems from "../../data/emojiItems";
 
 const Journal = () => {
   const dispatch = useDispatch();
-  const { allEvent } = useSelector((store) => store.event);
-  console.log("allevent:", allEvent);
+  dispatch(getAllEvents());
 
-  useEffect(() => {
-    dispatch(getAllEvents());
-  }, []);
   return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion>
-    </div>
+    <>
+      {emojiItems.map((emojiItem) => {
+        return (
+          <JournalCards
+            key={emojiItem.id}
+            emotion={emojiItem.emotions}
+            imageSrc={emojiItem.imageSrc}
+          />
+        );
+      })}
+    </>
   );
 };
 
