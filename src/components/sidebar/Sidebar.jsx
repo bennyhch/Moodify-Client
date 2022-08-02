d8aeafcee312fef646c6db8a1489c48975c6fa0d
Ben Ho <bennyhch@gmail.com>
2022-08-02 15:41:26 +0100

    sidebar styling done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
 import axios from "axios";
+import styles from "./sidebar.module.css";
+import { RiDashboardFill } from "react-icons/ri";
+import { ImClipboard, ImStatsDots } from "react-icons/im";
+import { BsJournalCheck } from "react-icons/bs";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -12,16 +16,48 @@ const Sidebar = () => {
   };
 
   return (
-    <nav>
-      <NavLink to="/">Dashboard</NavLink>
-      <NavLink to="/logger">Logger</NavLink>
-      <NavLink to="/statistics">Statistics</NavLink>
-      <NavLink to="/journal">Journal</NavLink>
-      <button onClick={logoutHandler}>
-        <FiLogOut />
-      </button>
+    <nav className={styles.navLinkContainer}>
+      <div className={styles.logoContainer}>
+        <img src={process.env.PUBLIC_URL + "assets/logo2.png"} alt="logo" />
+      </div>
+
+      <NavLink to="/" className={styles.navbtnContainer}>
+        <RiDashboardFill size={30} />
+        Dashboard
+      </NavLink>
+
+      <NavLink to="/logger" className={styles.navbtnContainer}>
+        <ImClipboard size={30} />
+        Logger
+      </NavLink>
+
+      <NavLink to="/statistics" className={styles.navbtnContainer}>
+        <ImStatsDots size={30} />
+        Statistics
+      </NavLink>
+
+      <NavLink to="/journal" className={styles.navbtnContainer}>
+        <BsJournalCheck size={30} />
+        Journal
+      </NavLink>
+
+      <NavLink
+        onClick={logoutHandler}
+        to="/"
+        className={styles.navbtnContainer}
+      >
+        <FiLogOut size={30} type="button" />
+        Logout
+      </NavLink>
     </nav>
   );
 };
 
 export default Sidebar;
+
+/* <div className={styles.navbtnContainer}>
+        <span>
+          <FiLogOut onClick={logoutHandler} size={30} type="button" />
+        </span>
+        Logout
+      </div> */
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
+import styles from "./sharedLayout.module.css";
 
 const SharedLayout = () => {
   return (
-    <>
-      <Sidebar />
-      <Outlet />
-    </>
+    <main className={styles.dashboard}>
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={styles.dashboardPage}>
+        <Outlet />
+      </div>
+    </main>
   );
 };
 
